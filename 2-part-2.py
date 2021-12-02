29cba0e5ba7474ff54076f6d66729abaefa58132
David Read <david.read@digital.justice.gov.uk>
2021-12-02 10:29:01 +0000

    Did Day 2 part 2

--- 2-part-2.py
+++ 2-part-2.py
@@ -1,4 +1,4 @@
-horizontal_position = depth = 0
+horizontal_position = depth = aim = 0
 with open('2-input.txt', 'r') as f:
     for line in f:
         action, value = line.split()  # e.g. line = 'forward 5'
@@ -6,10 +6,11 @@ with open('2-input.txt', 'r') as f:
 
         if action == 'forward':
             horizontal_position += value
+            depth += aim * value
         elif action == 'up':
-            depth -= value
+            aim -= value
         elif action == 'down':
-            depth += value
-        print(line, f'horizontal: {horizontal_position} depth: {depth}')
-        
+            aim += value
+        print(line, f'horizontal: {horizontal_position} aim: {aim} depth: {depth}')
+
 print(f'h * d = {horizontal_position * depth}')
