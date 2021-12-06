9e255299880cceeb3983e23a563edbac730632cd
David Read <david.read@digital.justice.gov.uk>
2021-12-06 10:22:32 +0000

    Day 6 attempt

---
+++ day6/tests.py
@@ -0,0 +1,40 @@
+import unittest
+
+from lantern import day, line_to_ints
+
+
+class TestLantern(unittest.TestCase):
+    def test_count_down(self):
+        self.assertEqual(
+            day([3,4,3,1,2]),
+                [2,3,2,0,1])
+
+    def test_examples(self):
+        examples = '''
+2,3,2,0,1
+1,2,1,6,0,8
+0,1,0,5,6,7,8
+6,0,6,4,5,6,7,8,8
+5,6,5,3,4,5,6,7,7,8
+4,5,4,2,3,4,5,6,6,7
+3,4,3,1,2,3,4,5,5,6
+2,3,2,0,1,2,3,4,4,5
+1,2,1,6,0,1,2,3,3,4,8
+0,1,0,5,6,0,1,2,2,3,7,8
+6,0,6,4,5,6,0,1,1,2,6,7,8,8,8
+5,6,5,3,4,5,6,0,0,1,5,6,7,7,7,8,8
+4,5,4,2,3,4,5,6,6,0,4,5,6,6,6,7,7,8,8
+3,4,3,1,2,3,4,5,5,6,3,4,5,5,5,6,6,7,7,8
+2,3,2,0,1,2,3,4,4,5,2,3,4,4,4,5,5,6,6,7
+1,2,1,6,0,1,2,3,3,4,1,2,3,3,3,4,4,5,5,6,8
+0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8
+6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8
+'''.strip().split()
+        examples = [line_to_ints(line) for line in examples]
+        yesterday = examples[0]
+        for today in examples[1:]:
+            self.assertEqual(day(yesterday), today)
+            yesterday = today
+
+if __name__ == '__main__':
+    unittest.main()
