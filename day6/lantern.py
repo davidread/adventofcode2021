7d2124009176d13baadf3cd84a0f1b5578c6d111
David Read <david.read@digital.justice.gov.uk>
2021-12-06 11:04:49 +0000

    Day 6 refactored for part 2

--- day6/lantern.py
+++ day6/lantern.py
@@ -1,28 +1,31 @@
+from collections import Counter
+
+
 def day(fishes):
-    out = fishes.copy()
-    for i, fish in enumerate(fishes):
-        if fish == 0:
-            fish = 6
-            out.append(8)
+    # store *counts* of fishes at each timer value
+    # (the alternative is to store a timer per fish, which doesn't scale)
+    out = Counter()
+    for timer, count in dict(fishes).items():
+        if timer == 0:
+            out.update({6: count, 8: count})
         else:
-            fish -= 1
-        
-        out[i] = fish
+            out.update({timer - 1: count})
+
     # print(out)
     return out
 
 
-def line_to_ints(line):
-    return [int(num) for num in line.split(',')]
+def line_to_counts(line):
+    return Counter(int(num) for num in line.split(','))
 
 
 if __name__ == '__main__':
     input = '3,4,1,1,5,1,3,1,1,3,5,1,1,5,3,2,4,2,2,2,1,1,1,1,5,1,1,1,1,1,3,1,1,5,4,1,1,1,4,1,1,1,1,2,3,2,5,1,5,1,2,1,1,1,4,1,1,1,1,3,1,1,3,1,1,1,1,1,1,2,3,4,2,1,3,1,1,2,1,1,2,1,5,2,1,1,1,1,1,1,4,1,1,1,1,5,1,4,1,1,1,3,3,1,3,1,3,1,4,1,1,1,1,1,4,5,1,1,3,2,2,5,5,4,3,1,2,1,1,1,4,1,3,4,1,1,1,1,2,1,1,3,2,1,1,1,1,1,4,1,1,1,4,4,5,2,1,1,1,1,1,2,4,2,1,1,1,2,1,1,2,1,5,1,5,2,5,5,1,1,3,1,4,1,1,1,1,1,1,1,4,1,1,4,1,1,1,1,1,2,1,2,1,1,1,5,1,1,3,5,1,1,5,5,3,5,3,4,1,1,1,3,1,1,3,1,1,1,1,1,1,5,1,3,1,5,1,1,4,1,3,1,1,1,2,1,1,1,2,1,5,1,1,1,1,4,1,3,2,3,4,1,3,5,3,4,1,4,4,4,1,3,2,4,1,4,1,1,2,1,3,1,5,5,1,5,1,1,1,5,2,1,2,3,1,4,3,3,4,3'
-    fishes = line_to_ints(input)
+    fishes = line_to_counts(input)
     for i in range(80):
         fishes = day(fishes)
-    print('After 80 days: ', len(fishes))
+    print('After 80 days: ', fishes.total())
 
     for i in range(256 - 80):
         fishes = day(fishes)
-    print('After 256 days: ', len(fishes))
+    print('After 256 days: ', fishes.total())
--- day6/tests.py
+++ day6/tests.py
@@ -1,13 +1,28 @@
 import unittest
 
-from lantern import day, line_to_ints
+from lantern import day, line_to_counts
 
 
 class TestLantern(unittest.TestCase):
     def test_count_down(self):
         self.assertEqual(
-            day([3,4,3,1,2]),
-                [2,3,2,0,1])
+            day(line_to_counts('3')),
+                line_to_counts('2'))
+
+    def test_count_down_consecutive(self):
+        self.assertEqual(
+            day(line_to_counts('3,2')),
+                line_to_counts('2,1'))
+
+    def test_count_downs(self):
+        self.assertEqual(
+            day(line_to_counts('3,4,3,1,2')),
+                line_to_counts('2,3,2,0,1'))
+
+    def test_spawn(self):
+        self.assertEqual(
+            day(line_to_counts('0')),
+                line_to_counts('6,8'))
 
     def test_examples(self):
         examples = '''
@@ -30,7 +45,7 @@ class TestLantern(unittest.TestCase):
 0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8
 6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8
 '''.strip().split()
-        examples = [line_to_ints(line) for line in examples]
+        examples = [line_to_counts(line) for line in examples]
         yesterday = examples[0]
         for today in examples[1:]:
             self.assertEqual(day(yesterday), today)
