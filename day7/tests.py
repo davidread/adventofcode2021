786aebe142b100c889be401003c5418154ac6860
David Read <david.read@digital.justice.gov.uk>
2021-12-07 12:17:03 +0000

    Did part 2

--- day7/tests.py
+++ day7/tests.py
@@ -2,14 +2,24 @@ import unittest
 
 import main
 
-class TestLeastFuel(unittest.TestCase):
+class TestLeastFuelPart1(unittest.TestCase):
     def test_simple(self):
-        self.assertEqual(main.calc_least_fuel([1, 2, 3]),
+        self.assertEqual(main.calc_least_fuel_part1([1, 2, 3]),
                          (2, 2))
 
     def test_example(self):
-        self.assertEqual(main.calc_least_fuel(main.str_to_ints('16,1,2,0,4,2,7,1,2,14')),
+        self.assertEqual(main.calc_least_fuel_part1(main.str_to_ints('16,1,2,0,4,2,7,1,2,14')),
                          (2, 37))
+
+
+class TestLeastFuelPart2(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(main.calc_least_fuel_part2([1, 2, 3]),
+                         (2, 2))
+
+    def test_example(self):
+        self.assertEqual(main.calc_least_fuel_part2(main.str_to_ints('16,1,2,0,4,2,7,1,2,14')),
+                         (5, 168))
 
 
 if __name__ == '__main__':
